5bcc2db2b36ea9b118f0c2f3bc28386dd45f264b
andrewhwaller <andrewhwaller@gmail.com>
2020-01-21 23:39:11 -0600

    Add seed data for testing

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,5 +1,4 @@
 class Quote < ApplicationRecord
     validates :name, :user_id, :text, presence: true
-    validates :source_publication_year, length: { is: 4 }
     belongs_to :user
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Quote.delete_all
+User.delete_all
+
+5.times do
+    user = User.create! email: Faker::Internet.email, password: 'locadex1234'
+    puts "Created a new user: #{user.email}"
+
+    2.times do
+        quote = Quote.create!(
+            name: Faker::Educator.subject, 
+            text: Faker::Quote.yoda,
+            source_title: Faker::Book.title,
+            source_author: Faker::Book.author,
+            source_page_number: Faker::Number.number(digits: 2),
+            source_publisher: Faker::Book.publisher,
+            source_publication_year: Faker::Number.between(1918,2018),
+            user_id: user.id
+        )
+        puts "Created a new quote: #{quote.name} from #{quote.source_author}"
+    end
+end
