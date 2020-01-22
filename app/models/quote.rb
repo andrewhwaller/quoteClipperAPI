02a41cef46a44b265e48bdc07440becd1abe268e
andrewhwaller <andrewhwaller@gmail.com>
2020-01-21 21:55:03 -0600

    Add validations for quote model

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,3 +1,5 @@
 class Quote < ApplicationRecord
-  belongs_to :user
+    validates :name, :user_id, :text, presence: true
+    validates :source_publication_year, numericality: { equal_to: 4 }
+    belongs_to :user
 end
--- test/models/quote_test.rb
+++ test/models/quote_test.rb
@@ -1,7 +1,11 @@
 require 'test_helper'
 
 class QuoteTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+    test "should have a four digit publication year" do
+        quote = quotes(:one)
+        quote.source_publication_year = 199
+        assert_not quote.valid?
+    end
+
 end
